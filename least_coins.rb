be75f1391e64b610ebfeb7b9cb6974ad377f3678
Mary White <maryewhite2022@gmail.com>
2018-07-13 16:38:08 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,36 +1,43 @@
 def least_coins(cents)
+ coin_types = {
+   :quarters => 0,
+   :dimes    => 0,
+   :nickels  => 0,
+   :pennies  => 0
+ }
 
-coin_types = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
+ quarters = cents/25
+  if quarters == 0
+     coin_types[:quarters] = 0
+  else quarters != 0
+     coin_types[:quarters] = quarters
+     cents = cents - quarters*25
+  end
 
-quarters=cents/25
-if quarters ==0
-  coin_types[:quarters]==0
-elsif quarters!=0
-  coin_types[:quarters]==
-  cents =cents -25
+ dimes = cents/10
+  if dimes == 0
+     coin_types[:dimes] = 0
+  else dimes != 0
+     coin_types[:dimes] = dimes
+     cents = cents - dimes*10
+  end
+
+ nickels = cents/5
+  if nickels == 0
+     coin_types[:nickels] = 0
+  else nickels != 0
+     coin_types[:nickels] = nickels
+     cents = cents - nickels*5
+  end
+
+ pennies = cents
+  if pennies == 0
+     coin_types[:pennies] = 0
+  else pennies != 0
+     coin_types[:pennies] = pennies
+  end
+
+ puts coin_types
+coin_types
+
 end
-
-dimes =cents/10
-if dimes ==0
-  coin_types[:dimes]==0
-elsif dimes !=0
-  cents = cents -10
-end
-
-nickels =cents/5
-if nickels ==0
-  coin_types[:nickels]==0
-elsif nickels !=0
-  cents == cents -5
-end
-
-pennies ==cents/1
-if pennies==0
-  coin_types[:pennies]==0
-elsif pennies !=0
-  cents = cents - 1
-end
-
-end
-
-puts coin_types(29)
